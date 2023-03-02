c90ccc3f2ec0e649f669a007ac0f21cf1fcc7abd
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-02 23:22:32 +0600

    middlewares added

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const express = require("express");
 //app
 const app = express();
 
+//middlewares
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the home directory " });
+});
+
 //port
 const PORT = process.env.PORT || 4000;
 
