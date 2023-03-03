f8c11deca081b4a993a2c5d2c597a9c01f181e50
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 02:02:06 +0600

    error handle

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -30,11 +30,44 @@ const getSingleProject = async (req, res) => {
 //post a single project
 
 const postProject = async (req, res) => {
-  const data = req.body;
+  const { title, tech, budget, duration, manager, dev } = req.body;
+
+  let emptyFields = [];
+
+  if (!title) {
+    emptyFields.push("title");
+  }
+
+  if (!tech) {
+    emptyFields.push("tech");
+  }
+
+  if (!budget) {
+    emptyFields.push("budget");
+  }
+
+  if (!duration) {
+    emptyFields.push("duration");
+  }
+
+  if (!manager) {
+    emptyFields.push("manager");
+  }
+
+  if (!dev) {
+    emptyFields.push("dev");
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
+  }
+
   try {
     //create document in mongodb
     const project = await Project.create({
-      ...data,
+      ...req.body,
     });
     res.status(200).json(project);
   } catch (err) {
