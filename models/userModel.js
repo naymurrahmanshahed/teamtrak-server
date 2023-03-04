a8cc04e267d10e81da34af2f49c8a9ec50da9587
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 13:53:01 +0600

    validator added

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -16,6 +17,23 @@ const userSchema = new Schema({
 
 //manual method create
 userSchema.statics.signUp = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  //check if the email is valid
+  if (!validator.isEmail(email)) {
+    throw Error("Please Enter Valid Email");
+  }
+
+  //password check
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Please make sure your password is at least 8 characters long and includes at least one uppercase letter, one lowercase letter, one number, and one special character"
+    );
+  }
+
   //exit email in mongodb
   const exist = await this.findOne({ email });
 
