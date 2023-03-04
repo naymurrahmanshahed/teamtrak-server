590f5cbb225822344c46df3ff60b8cfffe39d345
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2023-03-04 12:52:50 +0600

    bcrypt added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,3 @@
-const mongoose = require("mongoose");
-
 const User = require("../models/userModel");
 
 //Login
@@ -11,7 +9,15 @@ const loginUser = async (req, res) => {
 //signup
 
 const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signUp(email, password);
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -13,6 +13,27 @@ const userSchema = new Schema({
     required: true,
   },
 });
+
+//manual method create
+userSchema.statics.signUp = async function (email, password) {
+  //exit email in mongodb
+  const exist = await this.findOne({ email });
+
+  if (exist) {
+    throw Error("Email already used");
+  }
+
+  //password encrypt
+  const salt = await bcrypt.genSalt(10);
+
+  const hash = await bcrypt.hash(password, salt);
+
+  //create a user
+
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 //create model
 module.exports = mongoose.model("User", userSchema);
